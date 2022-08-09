bef55df7e27d75793b8b8524d21a5a192543a42d
Martin Kobetic <mkobetic@users.noreply.github.com>
2022-08-09 17:07:48 -0400

    grpc

--- go/generate.go
+++ go/generate.go
@@ -8,5 +8,5 @@ import (
 )
 
 //go:generate go install google.golang.org/grpc/cmd/protoc-gen-go-grpc
-//go:generate protoc --proto_path=../proto -I=. -I=../build/tmp/vendor --go_out=. --go-grpc_out=. --grpc-gateway_out=generate_unbound_methods=true:. ../proto/message_api/v1/message_api.proto ../proto/authn/v1/authn.proto
-//go:generate protoc --proto_path=../proto -I=. -I=../build/tmp/vendor --go_out=. ../proto/authn/v1/authn.proto
+//go:generate protoc --proto_path=../proto -I=. -I=../build/tmp/vendor --go_out=. --go-grpc_out=. --grpc-gateway_out=generate_unbound_methods=true:. ../proto/message_api/v1/message_api.proto
+//go:generate protoc --proto_path=../proto -I=. -I=../build/tmp/vendor --go_out=. --go-grpc_out=. --grpc-gateway_out=generate_unbound_methods=true:. ../proto/authn/v1/authn.proto
