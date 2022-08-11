99d27ab59486c9ca223fbdf9b5f19996c7efc894
Nicholas Molnar <nicholas@xmtp.com>
2022-08-11 08:22:17 -0700

    feat: import client protos
    Also lints

--- ts/index.ts
+++ ts/index.ts
@@ -1,3 +1,6 @@
-export * from "./message_api/v1/message_api.pb";
+export * as messageApi from "./message_api/v1/message_api.pb";
 export * as fetcher from "./fetch.pb";
 export * as authn from "./message_api/v1/authn.pb";
+export * as xmtpEnvelope from "./message_contents/xmtp_envelope.pb";
+export * as composite from "./message_contents/composite.pb";
+export * as privateKey from "./message_contents/private_key.pb";
