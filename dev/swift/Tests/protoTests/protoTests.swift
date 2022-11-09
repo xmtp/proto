3702b97395da4e2edc8b0243f9cd1526ca7615ce
Pat Nakajima <patnakajima@gmail.com>
2022-11-09 22:37:32 +0000

    feat: generate swift files
    Generates swift files. I'm checking them in because that's how SPM seems to want it ([similar to go](https://github.com/xmtp/proto/pull/4#issuecomment-1198249166)).
    
    One small thing I ran into is that I wasn't able to use the `macos` GitHub action environment for tests because that doesn't include Docker ([more here](https://github.com/orgs/community/discussions/25777#discussioncomment-3249229)). While I think most people (everyone?) using this library will be on a Mac, there's nothing really platform specific about the generated code so I think we're ok here.

---
+++ dev/swift/Tests/protoTests/protoTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import XMTPProto
+
+final class protoTests: XCTestCase {
+    func testTypesAreAvailable() throws {
+			// This is just a bare minimum test to make sure the types are generated
+			XCTAssertEqual(Xmtp_MessageApi_V1_SortDirection.unspecified.rawValue, 0)
+    }
+}
