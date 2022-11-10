7ffdcf50bb6bb5b00c5463e3aa46e623208119bb
Martin Kobetic <mkobetic@users.noreply.github.com>
2022-11-10 16:51:38 -0500

    feat: rename/split xmtp_envelope.proto

--- dart/lib/xmtp_proto.dart
+++ dart/lib/xmtp_proto.dart
@@ -2,7 +2,8 @@ library xmtp_proto;
 
 export 'src/message_api/v1/message_api.pbgrpc.dart';
 export 'src/message_api/v1/authn.pb.dart';
-export 'src/message_contents/xmtp_envelope.pb.dart';
+export 'src/message_contents/message.pb.dart';
+export 'src/message_contents/content.pb.dart';
 export 'src/message_contents/composite.pb.dart';
 export 'src/message_contents/private_key.pb.dart';
 export 'src/message_contents/public_key.pb.dart';
