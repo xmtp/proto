8759cb022b8d2efbe1293c64a851bc1828ca6a7e
Nicholas Molnar <nicholas@xmtp.com>
2022-11-14 11:09:05 -0800

    fix: re-add xmtp_proto.dart

---
+++ dart/lib/xmtp_proto.dart
@@ -0,0 +1,12 @@
+library xmtp_proto;
+
+export 'src/message_api/v1/message_api.pbgrpc.dart';
+export 'src/message_api/v1/authn.pb.dart';
+export 'src/message_contents/xmtp_envelope.pb.dart';
+export 'src/message_contents/composite.pb.dart';
+export 'src/message_contents/private_key.pb.dart';
+export 'src/message_contents/public_key.pb.dart';
+export 'src/message_contents/signature.pb.dart';
+export 'src/message_contents/ciphertext.pb.dart';
+export 'src/message_contents/contact.pb.dart';
+export 'src/message_contents/invitation.pb.dart';
