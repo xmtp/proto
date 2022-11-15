94499b19685885941739ac48b22d886981e7b5e4
Pat Nakajima <patnakajima@gmail.com>
2022-11-14 18:03:10 -0800

    feat: generate gRPC types for swift
    We'll want these to actually use the types generated in #23.

--- Package.swift
+++ Package.swift
@@ -16,13 +16,14 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-protobuf.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/grpc/grpc-swift", .upToNextMajor(from: "1.0.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "XMTPProto",
-            dependencies: [.product(name: "SwiftProtobuf", package: "swift-protobuf")],
+            dependencies: [.product(name: "SwiftProtobuf", package: "swift-protobuf"), .product(name: "GRPC", package: "grpc-swift")],
             path: "swift"
         ),
         .testTarget(
