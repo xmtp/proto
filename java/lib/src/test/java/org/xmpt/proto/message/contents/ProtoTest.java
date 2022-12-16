90e3f719b5300ef7bc799c15d25bcd35a2cfe638
Naomi Plasterer <nplasterer@rewatch.com>
2022-12-16 14:19:15 -0800

    small tweak to the test

--- java/lib/src/test/java/org/xmpt/proto/message/contents/ProtoTest.java
+++ java/lib/src/test/java/org/xmpt/proto/message/contents/ProtoTest.java
@@ -1,13 +1,11 @@
-package org.xmpt.proto;
+package org.xmpt.proto.message.contents;
 
 import org.junit.Test;
-import org.xmtp.proto.message.contents.MessageOuterClass.Message;
-
+import org.xmtp.proto.message.contents.*;
 import static org.junit.Assert.*;
 
 public class ProtoTest {
     @Test public void versionMethodReturnsTrue() {
-        Message message = new MessageV2();
-        assertTrue(true, message.hasV2());
+        assertEqual(Content.Compression.COMPRESSION_DEFLATE.getNumber(), 0);
     }
 }
