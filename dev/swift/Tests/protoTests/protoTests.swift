75755ef99e7c92bb835d218221f7cfad1ff9f726
Nicholas Molnar <65710+neekolas@users.noreply.github.com>
2024-04-15 12:13:06 -0700

    build: cleanup unused build steps

--- dev/swift/Tests/protoTests/protoTests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-@testable import XMTPProto
-
-final class protoTests: XCTestCase {
-    func testTypesAreAvailable() throws {
-        // This is just a bare minimum test to make sure the types are generated
-        XCTAssertEqual(Xmtp_MessageApi_V1_SortDirection.unspecified.rawValue, 0)
-    }
-}
